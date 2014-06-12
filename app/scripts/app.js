bd268d5f314a57e16a906ca63f7092344fa79c23
Evgeniy Korsunov <korsunov@iknow.travel>
2014-06-12 19:41:03 +0300

    creating routes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,15 +1,47 @@
 define([
-  'backbone',
-  'mainpage'
-  ], function(
-    Backbone,
-    mainpageModule
-  ) {
+    'backbone',
+    'mainpage'
+    ], function(
+        Backbone,
+        mainpageModule
+    ) {
 
-  var App = Backbone.Router.extend({
+    var App = Backbone.Router.extend({
+        routes: {
+            '': 'index',
+            'add_order': 'order',
+            'add_order/:price/:image': 'order',
+            'add_comment': 'comment',
+            'catalog/:name': 'catalog',
+            'catalog/:name/:id': 'gallery'
+        },
 
-  });
+        initialize: function() {
+            console.log('init');
+        },
 
-  return new App;
+        index: function() {
+            console.log('index');
+        },
+
+        order: function(price, img) {
+            console.log(price + ' ' + img)
+            console.log('add order');
+        },
+
+        comment: function() {
+            console.log('add comment');
+        },
+
+        catalog: function(name) {
+            console.log('catalog ' + name);
+        },
+
+        gallery: function(name, id) {
+            console.log('catalog ' + name + ' ' + id);
+        }
+    });
+
+    return new App;
 
 });
